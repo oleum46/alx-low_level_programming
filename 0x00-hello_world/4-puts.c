2042aca986d5a84398d09b4580e8260a43bf0063
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-05 08:48:25 -0700

    betty style now implemented

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programming is like building multilingual puzzle");
+{
+	puts("\"programming is like building multilingual puzzle");
 	return(0);
 }
