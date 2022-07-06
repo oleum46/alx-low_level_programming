fe9528dcf50019d8e84c9ba006981fb01851e5ff
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-05 18:50:11 -0700

    white space modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - prints out the last part of a quote in a standarderror
+ * main - prints out the last part of a quote in a standar error
+ *
  *Return: 1 if Success.
  */
 
 int main(void)
 	
 {
-	write(2, "and that piece of art is useful - Dora Ko		rpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return(1);
 }
