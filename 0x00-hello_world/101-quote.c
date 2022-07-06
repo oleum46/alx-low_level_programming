525f4429524eb3e267cffa6ed310e1bd1a4c11f1
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-05 18:02:06 -0700

    Added a C program that prints

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints out the lqst part of a quote in a standarderror
+ *
+ *Return: 1 if Success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpa, 2015-10-19\n", 58);
+	return(1);
+}
