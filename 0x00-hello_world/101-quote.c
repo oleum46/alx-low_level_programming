1fa6671b02f92faf9cf025ab53ea3fe5a670fafe
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-05 18:23:57 -0700

    spelling fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints out the lqst part of a quote in a standarderror
+ * main - prints out the last part of a quote in a standarderror
  *
  *Return: 1 if Success.
  */
 int main(void)
+	
 {
-	write(2, "and that piece of art is useful - Dora Korpa, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return(1);
 }
