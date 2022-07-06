0a53beafa029eb101fdeacf60249e5fc5f2d6c38
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-06 12:11:35 -0700

    betty style use

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
- * main - prints out the last part of a quote in a standar error
+ * main - prints out the last part of a quote in a standard error
  *
- *Return: 1 if Success.
+ *Return: Always 0 (Success)
  */
 int main(void)	
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return(1);
 }
