4e7233d95170c395a9d34c8b5425db3d3ec5e442
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-05 18:37:04 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
  * main - prints out the last part of a quote in a standarderror
- *
  *Return: 1 if Success.
  */
+
 int main(void)
 	
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Ko		rpar, 2015-10-19\n", 58);
 	return(1);
 }
