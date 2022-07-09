d1fb4bb645d18ca2be1ede092e63e40bde017bdf
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-09 14:42:41 -0700

    print alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in reverse order
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
