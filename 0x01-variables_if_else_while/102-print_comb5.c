7291075c3333aac7b1038eba184920246147aee8
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-10 03:34:19 -0700

    A program to print two two digit combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print two two digits combination
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 99; x++)
+	{
+		for (y =0; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x != 98 || y != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
