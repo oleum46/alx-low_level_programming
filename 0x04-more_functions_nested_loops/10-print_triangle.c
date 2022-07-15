a8c2fcf5d7aa65ba35c39885033556d50794e663
Osarumwense Eghosa <eghosasami@gmail.com>
2022-07-15 12:34:17 -0700

    A program that finds and prints the largest prime factor of the number

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,8 +10,7 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
 		int i, j;
 
@@ -25,6 +24,7 @@ void print_triangle(int size)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
